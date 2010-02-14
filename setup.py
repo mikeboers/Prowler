e672adaea083f6fc05923f0a851ee1eb9932d9d4
Mike Boers <github@mikeboers.com>
2010-02-14 00:05:59 -0500

    Ready for distribution.
    - Added license.
    - Wrote `setup.py`.
    - Renamed to `prowler`.

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name='prowler',
+    version='1.0b',
+    description='Tools for interacting with prowl notification service.',
+    url='http://github.com/mikeboers/PyProwler',
+    py_modules=['prowler'],
+    
+    author='Mike Boers',
+    author_email='nitrogen@mikeboers.com',
+    license='New BSD License',
+)
